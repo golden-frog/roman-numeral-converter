e8bc6d15459156a751295479ecc030255faf14a0
puntahamra <raf.verbeelen@icloud.com>
2018-07-21 16:06:47 +0200

    Solution.

--- code.js
+++ code.js
@@ -5,7 +5,24 @@
 All roman numerals answers should be provided in upper-case. */
 
 function convertToRoman(num) {
- return num;
+
+  // 2 arrays of decimals and their corresponding romans
+
+  const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1], romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  // The roman output, starts with an empty string
+
+  let roman = '';
+
+  decimals.forEach(function(decimal) {
+    while (decimal <= num) {
+      roman += romans[decimals.indexOf(decimal)];
+      num -= decimal;
+    }
+  });
+
+  return roman;
+
 }
 
-convertToRoman(36);
+console.log(convertToRoman(36));
